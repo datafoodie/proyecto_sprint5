fa08f687368cc6850f8e220dffb649514b662fe7
datafoodie <datafoodies@gmail.com>
2024-06-20 23:22:58 -0600

    el archivo principal

---
+++ app.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+st.header('Lanzar una moneda')
+
+car_data = pd.read_csv('vehicles_us.csv')  # leer los datos
+hist_button = st.button('Construir histograma')  # crear un botón
+disp_button = st.button('Construir un grafico de dispersión')
+
+if hist_button:  # al hacer clic en el botón
+    # escribir un mensaje
+    st.write(
+        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+
+    # crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+if disp_button:  # al hacer clic en el botón
+    # escribir un mensaje
+    st.write(
+        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+
+    # crear un histograma
+    fig = px.scatter(car_data, x="odometer", y="price")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
